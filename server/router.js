ead29bb602f82be7f16506e71b5703a283cbad78
Shenchengfu <sf2559@g.rit.edu>
2023-11-05 17:49:11 -0500

    c

--- server/router.js
+++ server/router.js
@@ -5,13 +5,13 @@ const router = (app) => {
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
-  app.get('/signup', mid.requiresSecure, mid.requiresLogout,controllers.Account.signupPage);
-  app.post('/signup', mid.requiresSecure, mid.requiresLogout,controllers.Account.signup);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
 
-  app.get('/logout', mid.requiresLogin,controllers.Account.logout);
-  
-  app.get('/maker', mid.requiresLogin,controllers.Domo.makerPage);
-  app.post('/maker', mid.requiresLogin,controllers.Domo.makeDomo);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.makeDomo);
 
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
